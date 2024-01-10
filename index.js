1660756dd919b92569afb60db59609888eddf3dc
Kelly Rogers <kellyerogers92@gmail.com>
2024-01-10 16:28:02 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(){
+    let existingText = document.getElementById('text')
+
+    existingText.textContent = "This is really cool!"
+})
+
+
+//Code your solution in index.js. First, set up a DOMContentLoaded event 
+//listener to detect when the HTML page has loaded and the document is ready to be manipulated. 
+//Use the event's callback function to target the paragraph element with id="text" and replace 
+// the text with "This is really cool!"
+//Note: Using the innerTextLinks to an external site. property to modify DOM element content will not work for this lab. Use textContentLinks to an external site. or innerHTMLLinks to an external site. instead.
+
